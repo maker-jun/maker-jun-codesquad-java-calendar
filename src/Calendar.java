56170e7ac8a4c405c7fadba788b8fd54256d2c79
maker-jun <nicejun0524@gmail.com>
2020-04-05 17:26:12 +0900

    한글깨짐 고쳐봄

--- src/Calendar.java
+++ src/Calendar.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
 
-		System.out.println("hello");
-		System.out.println("ÀÏ   ¿ù   È­   ¼ö   ¸ñ   ±İ   Åä");
+		System.out.println("ì¼   ì›”   í™”   ìˆ˜   ëª©   ê¸ˆ   í†   ");
 		System.out.println("--------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		System.out.println("================");
+
+		
+
+		
 
 
-
-		//Scanner s =new Scanner();
 	}
+	
 
 }
