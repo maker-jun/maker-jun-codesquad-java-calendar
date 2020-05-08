4302daf2662c1925f9bb7d031c90137953a2014b
maker-jun <nicejun0524@gmail.com>
2020-05-08 22:57:51 +0900

    Omplement fin max days of the month

--- src/Calendar.java
+++ src/Calendar.java
@@ -12,6 +12,13 @@ public class Calendar {
 		System.out.println("22 23 24 25 26 27 28");
 		System.out.println("================");
 
+		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("달을 입력하세요");
+		int month = scanner.nextInt();
+		
+		int[] maxDays = {31,28,31,30,31,30,31,31,30,31,30,31};
+		System.out.println(month+"달의 최대 일수는 : "+maxDays[month-1]);
 	}
 	
 
--- src/gugudan.java
+++ src/gugudan.java
@@ -4,12 +4,13 @@ public class gugudan {
 	public static void main(String[] args) {
 
 		gugudan g = new gugudan();
-		g.gugu1(19);
+		//g.gugu1(19);
 		Scanner scan = new Scanner(System.in);
 		String str= scan.nextLine();
 		//int num=scan.nextInt();
 	
-		g.gugu2(str);
+		//g.gugu2(str);
+		g.gugu3(str);
 	}
 	
 	public void gugu1(int n) {
@@ -28,7 +29,20 @@ public class gugudan {
 				System.out.println(i+" X "+j+" = "+i*j);
 			}
 		}
-		
 	}
 
+//	public String[][] gugu3(String s) {
+//	
+//		
+//		String[] splitS = s.split(",");
+//		int first = Integer.parseInt(splitS[0]);
+//		int second = Integer.parseInt(splitS[1]);
+//		
+//		for(int i = 1; i<=first;i++) {
+//			for(int j = 1; j<=second;j++) {
+//				String[][] str="";
+//			}
+//		}
+//		return str;
+//	}
 }
